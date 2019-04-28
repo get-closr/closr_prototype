e7905daea30e4358831d15b6850de43f6e6ceea9
Wan Yi <wanyizhou.z@gmail.com>
2019-04-28 23:13:14 +0800

    Working android flavors and firebase projects

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:closr_prototype/resources/app_config.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//TODO:Use as debug page to print firebase setup details before going to login page
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
@@ -12,15 +14,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   bool loading = false;
   TextEditingController _controller = TextEditingController();
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,13 +29,6 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Text(
                 "You are running ${AppConfig.of(context).buildFlavor} flavor."),
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
             loading ? CircularProgressIndicator() : Container(),
             TextField(
               controller: _controller,
@@ -51,11 +39,6 @@ class _MyHomePageState extends State<MyHomePage> {
             )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
